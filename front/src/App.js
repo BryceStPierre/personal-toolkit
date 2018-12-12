634a8bac1630e577f8e6c4a378bb61f2290eefc5
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-12-11 20:35:05 -0500

    Cleaned up front end and added Font Awesome support.

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -19,6 +18,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <FontAwesomeIcon icon="igloo" />
         </header>
       </div>
     );
